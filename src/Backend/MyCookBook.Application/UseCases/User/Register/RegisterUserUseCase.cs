21f90d6193ccaf1633103caf423c00cc6355b4fc
ericssendelima <ericssendelima@gmail.com>
2025-11-18 11:31:50 -0300

    feat: adding initial valitadion for request register user

--- src/Backend/MyCookBook.Application/UseCases/User/Register/RegisterUserUseCase.cs
+++ src/Backend/MyCookBook.Application/UseCases/User/Register/RegisterUserUseCase.cs
@@ -1,5 +1,6 @@
 ﻿using MyCookBook.Communication.Requests;
 using MyCookBook.Communication.Responses;
+using MyCookBook.Exceptions.ExceptionsBase;
 
 namespace MyCookBook.Application.UseCases.User.Register
 {
@@ -7,10 +8,33 @@ namespace MyCookBook.Application.UseCases.User.Register
   {
     public async Task<ResponseRegisterUserJson> Execute(RequestRegisterUserJson request)
     {
+      //Validação dos dados da request
+      await Validate(request);
+
+      //Mapear a request em uma entidade
+
+      //Criptografar a senha
+
+      //Salvar no banco de dados
+
       return new ResponseRegisterUserJson
       {
         Name = request.Name
       };
+    }
+
+    private async Task Validate(RequestRegisterUserJson request)
+    {
+      var validator = new RegisterUserValidator();
+
+      var result = validator.Validate(request);
+
+      if (result.IsValid == false)
+      {
+        var errorMessages = result.Errors.Select(e => e.ErrorMessage).ToList();
+
+        throw new ErrorOnValidationException(errorMessages);
+      }
     }
   }
 }
---
+++ src/Backend/MyCookBook.Application/UseCases/User/Register/RegisterUserValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using MyCookBook.Communication.Requests;
+
+namespace MyCookBook.Application.UseCases.User.Register
+{
+  public class RegisterUserValidator : AbstractValidator<RequestRegisterUserJson>
+  {
+    public RegisterUserValidator()
+    {
+      RuleFor(user => user.Name).NotEmpty();
+      RuleFor(user => user.Email).NotEmpty();
+      RuleFor(user => user.Password.Length).GreaterThanOrEqualTo(6);
+
+      When(user => string.IsNullOrEmpty(user.Email) == false, () =>
+      {
+        RuleFor(user => user.Email).EmailAddress();
+      });
+    }
+  }
+}
