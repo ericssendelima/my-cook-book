ceb2b8b03edf229c7b9f6158a8e861a4bb22cfa5
ericssendelima <ericssendelima@gmail.com>
2025-11-18 11:32:37 -0300

    feat: adding exceptions filter configurations

---
+++ src/Backend/MyCookBook.API/Middlewares/CultureMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+namespace MyCookBook.API.Middlewares
+{
+  public class CultureMiddleware(RequestDelegate next)
+  {
+    private readonly RequestDelegate _next = next;
+    public async Task Invoke(HttpContext context)
+    {
+      var supportedLanguages = CultureInfo.GetCultures(CultureTypes.AllCultures);
+      var requestedCulture = context.Request.Headers.AcceptLanguage.FirstOrDefault();
+
+      var cultureInfo = new CultureInfo("en");
+
+      if (string.IsNullOrWhiteSpace(requestedCulture) == false
+        && supportedLanguages.Any(c => c.Name.Equals(requestedCulture)))
+      {
+        cultureInfo = new CultureInfo(requestedCulture);
+      }
+
+      CultureInfo.CurrentCulture = cultureInfo; 
+      CultureInfo.CurrentUICulture = cultureInfo;
+
+      await _next(context);
+    }
+  }
+}
--- src/Backend/MyCookBook.API/Program.cs
+++ src/Backend/MyCookBook.API/Program.cs
@@ -1,13 +1,18 @@
+using MyCookBook.API.Filters;
+using MyCookBook.API.Middlewares;
 using MyCookBook.Application;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddMvc(options => options.Filters.Add(typeof(ExceptionFilter)));
 
 builder.Services.AddApplication();
 
@@ -16,9 +21,11 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
+
+app.UseMiddleware<CultureMiddleware>();
 
 app.UseHttpsRedirection();
 
