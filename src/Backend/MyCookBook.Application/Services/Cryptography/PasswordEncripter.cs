9bd9cefef3e7573e097d35eb9224f6815b3ad000
ericssendelima <ericssendelima@gmail.com>
2025-11-20 11:20:18 -0300

    feat: adding password encripter

---
+++ src/Backend/MyCookBook.Application/Services/Cryptography/PasswordEncripter.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace MyCookBook.Application.Services.Cryptography
+{
+  public class PasswordEncripter(string additionalKey)
+  {
+    private readonly string _additionalKey = additionalKey;
+
+    public string Encrypt(string password)
+    {
+      var newPassword = $"{password}{_additionalKey}";
+
+      var bytes = Encoding.UTF8.GetBytes(password);
+      var hashBytes = SHA512.HashData(bytes);
+
+      return StringBytes(hashBytes);
+    }
+
+    private static string StringBytes(byte[] bytes)
+    {
+      var sb = new StringBuilder();
+
+      foreach (byte b in bytes)
+      {
+        sb.Append(b.ToString("x2"));
+      }
+
+      return sb.ToString();
+    }
+  }
+}
