4f1126681a8e6e38697cac7b5adf38fe5f39426b
ericssendelima <ericssendelima@gmail.com>
2025-11-21 15:46:15 -0300

    feat: Preparing usecase to receive dependencies injection

--- src/Backend/MyCookBook.Application/DependencyInjectionExtension.cs
+++ src/Backend/MyCookBook.Application/DependencyInjectionExtension.cs
@@ -1,18 +1,40 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MyCookBook.Application.Services.AutoMapper;
+using MyCookBook.Application.Services.Cryptography;
 using MyCookBook.Application.UseCases.User.Register;
 
 namespace MyCookBook.Application
 {
   public static class DependencyInjectionExtension
   {
-    public static void AddApplication(this IServiceCollection services)
+    public static void AddApplication(this IServiceCollection services, IConfiguration configuration)
     {
+      AddPasswordEncripter(services, configuration);
+      AddAutoMapper(services);
       AddUseCases(services);
+    }
+
+    private static void AddAutoMapper(IServiceCollection services)
+    {
+      var autoMapper = new AutoMapper.MapperConfiguration(options =>
+      {
+        options.AddProfile(new AutoMapping());
+      }).CreateMapper();
+
+      services.AddScoped(options => autoMapper);
     }
 
     private static void AddUseCases(IServiceCollection services)
     {
       services.AddScoped<IRegisterUserUseCase, RegisterUserUseCase>();
+    }
+
+    private static void AddPasswordEncripter(IServiceCollection services, IConfiguration configuration)
+    {
+      var additionalKey = configuration.GetValue<string>("Settings:Password:AdditionalKey");
+
+      services.AddScoped(option => new PasswordEncripter(additionalKey!));
     }
   }
 }
--- src/Backend/MyCookBook.Application/UseCases/User/Register/RegisterUserUseCase.cs
+++ src/Backend/MyCookBook.Application/UseCases/User/Register/RegisterUserUseCase.cs
@@ -1,21 +1,51 @@
-﻿using MyCookBook.Communication.Requests;
+﻿using AutoMapper;
+using MyCookBook.Application.Services.Cryptography;
+using MyCookBook.Communication.Requests;
 using MyCookBook.Communication.Responses;
+using MyCookBook.Domain.Repositories;
+using MyCookBook.Domain.Repositories.User;
+using MyCookBook.Exceptions;
 using MyCookBook.Exceptions.ExceptionsBase;
 
 namespace MyCookBook.Application.UseCases.User.Register
 {
   public class RegisterUserUseCase : IRegisterUserUseCase
   {
+    private readonly IMapper _mapper;
+    private readonly PasswordEncripter _passwordEncripter;
+    private readonly IUserReadOnlyRepository _readOnlyRepository;
+    private readonly IUserWriteOnlyRepository _writeOnlyRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public RegisterUserUseCase(
+      IMapper mapper,
+      PasswordEncripter passwordEncripter,
+      IUnitOfWork unitOfWork,
+      IUserReadOnlyRepository readOnlyRepository,
+      IUserWriteOnlyRepository writeOnlyRepository
+      )
+    {
+      _mapper = mapper;
+      _passwordEncripter = passwordEncripter;
+      _readOnlyRepository = readOnlyRepository;
+      _writeOnlyRepository = writeOnlyRepository;
+      _unitOfWork = unitOfWork;
+    }
     public async Task<ResponseRegisterUserJson> Execute(RequestRegisterUserJson request)
     {
       //Validação dos dados da request
       await Validate(request);
 
       //Mapear a request em uma entidade
+      var user = _mapper.Map<Domain.Entities.User>(request);
 
       //Criptografar a senha
+      user.Password = _passwordEncripter.Encrypt(request.Password);
 
       //Salvar no banco de dados
+      await _writeOnlyRepository.Add(user);
+
+      await _unitOfWork.Commit();
 
       return new ResponseRegisterUserJson
       {
@@ -28,6 +58,13 @@ namespace MyCookBook.Application.UseCases.User.Register
       var validator = new RegisterUserValidator();
 
       var result = validator.Validate(request);
+
+      var emailExists = await _readOnlyRepository.ExistActiveUserWithEmail(request.Email);
+
+      if (emailExists)
+      {
+        result.Errors.Add(new FluentValidation.Results.ValidationFailure(string.Empty, ResourceMessagesException.EMAIL_ALREADY_REGISTERED));
+      }
 
       if (result.IsValid == false)
       {
